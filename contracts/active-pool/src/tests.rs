41ff26ff798af5f74f2532c27479f6e925262ffc
chinhnotional <eyescryptoinsights@gmail.com>
2022-06-19 04:49:35 +0700

    add some tests fot active pool contract (instantiate, set addresses, increase decrease usj debt)

--- contracts/active-pool/src/tests.rs
+++ contracts/active-pool/src/tests.rs
@@ -53,7 +53,160 @@ fn test_instantiate() {
         .query_wasm_smart(&active_pool_addr, &QueryMsg::GetParams {})
         .unwrap();
 
-    assert_eq!(
-        response.owner, Addr::unchecked(OWNER)
-    );
+    assert_eq!(response.owner, Addr::unchecked(OWNER));
+
+    assert_eq!(response.name, "Active Pool");
 }
+
+#[test]
+fn test_set_addresses() {
+    let mut app = App::default();
+
+    let msg = InstantiateMsg {
+        name: String::from("Active Pool"),
+        owner: OWNER.to_string(),
+    };
+
+    let active_pool_addr = instantiate_active_pool(&mut app, msg);
+
+    let set_addresses_msg = ExecuteMsg::SetAddresses {
+        borrower_operations_address: BO.to_string(),
+        default_pool_address: DP.to_string(),
+        stability_pool_address: SP.to_string(),
+        trove_manager_address: TM.to_string(),
+    };
+
+    let err: ContractError = app
+        .execute_contract(
+            Addr::unchecked(SOME),
+            active_pool_addr.clone(),
+            &set_addresses_msg,
+            &[],
+        )
+        .unwrap_err()
+        .downcast()
+        .unwrap();
+    assert_eq!(err, ContractError::UnauthorizedOwner {});
+
+    app.execute_contract(
+        Addr::unchecked(OWNER),
+        active_pool_addr.clone(),
+        &set_addresses_msg,
+        &[],
+    )
+    .unwrap();
+
+    let bo_address: Addr = app
+        .wrap()
+        .query_wasm_smart(
+            active_pool_addr.clone(),
+            &QueryMsg::GetBorrowerOperationsAddress {},
+        )
+        .unwrap();
+    assert_eq!(bo_address, Addr::unchecked(BO));
+
+    let tm_address: Addr = app
+        .wrap()
+        .query_wasm_smart(
+            active_pool_addr.clone(),
+            &QueryMsg::GetTroveManagerAddress {},
+        )
+        .unwrap();
+    assert_eq!(tm_address, Addr::unchecked(TM));
+
+    let sp_address: Addr = app
+        .wrap()
+        .query_wasm_smart(
+            active_pool_addr.clone(),
+            &QueryMsg::GetStabilityPoolAddress {},
+        )
+        .unwrap();
+    assert_eq!(sp_address, Addr::unchecked(SP));
+
+    let dp_address: Addr = app
+        .wrap()
+        .query_wasm_smart(
+            active_pool_addr.clone(),
+            &QueryMsg::GetDefaultPoolAddress {},
+        )
+        .unwrap();
+    assert_eq!(dp_address, Addr::unchecked(DP));
+}
+
+#[test]
+fn test_increase_decrease_usj_debt() {
+    let mut app = App::default();
+    let msg = InstantiateMsg {
+        name: String::from("Active Pool"),
+        owner: OWNER.to_string(),
+    };
+
+    let active_pool_addr = instantiate_active_pool(&mut app, msg);
+
+    let set_addresses_msg = ExecuteMsg::SetAddresses {
+        borrower_operations_address: BO.to_string(),
+        default_pool_address: DP.to_string(),
+        stability_pool_address: SP.to_string(),
+        trove_manager_address: TM.to_string(),
+    };
+
+    app.execute_contract(
+        Addr::unchecked(OWNER),
+        active_pool_addr.clone(),
+        &set_addresses_msg,
+        &[],
+    )
+    .unwrap();
+
+    let increase_usj_debt_msg = ExecuteMsg::IncreaseUSJDebt {
+        amount: Uint128::new(100u128),
+    };
+
+    let decrease_usj_debt_msg = ExecuteMsg::DecreaseUSJDebt {
+        amount: Uint128::new(50u128),
+    };
+
+    let err: ContractError = app
+        .execute_contract(
+            Addr::unchecked(SOME),
+            active_pool_addr.clone(),
+            &increase_usj_debt_msg,
+            &[],
+        )
+        .unwrap_err()
+        .downcast()
+        .unwrap();
+    assert_eq!(err, ContractError::CallerIsNeitherBONorTM {});
+
+    app.execute_contract(
+        Addr::unchecked(TM),
+        active_pool_addr.clone(),
+        &increase_usj_debt_msg,
+        &[],
+    )
+    .unwrap();
+
+    let usj_debt: Uint128 = app
+        .wrap()
+        .query_wasm_smart(active_pool_addr.clone(), &QueryMsg::GetUSJDebt {})
+        .unwrap();
+
+    assert_eq!(usj_debt, Uint128::new(100u128));
+
+    app.execute_contract(
+        Addr::unchecked(TM),
+        active_pool_addr.clone(),
+        &decrease_usj_debt_msg,
+        &[],
+    )
+    .unwrap();
+
+    let usj_debt: Uint128 = app
+        .wrap()
+        .query_wasm_smart(active_pool_addr.clone(), &QueryMsg::GetUSJDebt {})
+        .unwrap();
+
+    assert_eq!(usj_debt, Uint128::new(50u128));
+}
+
+
